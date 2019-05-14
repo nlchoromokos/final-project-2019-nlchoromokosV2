e64db3aae05f797a48b760c65282e2b81ae853b0
ejgabel <42478466+ejgabel@users.noreply.github.com>
2019-05-14 15:05:12 -0500

    dwaddwads

--- FinalProject/src/defaultPackage/GameObject.java
+++ FinalProject/src/defaultPackage/GameObject.java
@@ -1,57 +1,58 @@
  
 
 import java.awt.Graphics;
+//import java.awt.*;
 
 //going to be all the game objects, lets it clump the same kinds of objects into categories
 //all the 'this' is because it access the protected variables at the top
 
 public abstract class GameObject {
 
-	protected int x, y;
-	protected ID id;
-	protected int velX, velY;
-	
-	public GameObject(int x, int y, ID id) {
-		this.x = x;
-		this.y = y;
-		this.id = id;
-				
-	}
-	
-	public abstract void tick();
-	public abstract void render(Graphics g);
-	
+    protected int x, y;
+    protected ID id;
+    protected int velX, velY;
+    
+    public GameObject(int x, int y, ID id) {
+        this.x = x;
+        this.y = y;
+        this.id = id;
+        
+    }
+    
+    public abstract void tick();
+    public abstract void render(Graphics g);
+    
 //setters
-	public void setX(int x) {
-		this.x = x;
-	}
-	public void setY(int y) {
-		this.y = y;
-	}
-	public void setVelX(int velX) {
-		this.velX = velX;
-	}
-	public void setVelY(int velY) {
-		this.velY = velY;
-	}
-	public void setID(ID id) {
-		this.id = id;
-	}
+    public void setX(int x) {
+        this.x = x;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public void setVelX(int velX) {
+        this.velX = velX;
+    }
+    public void setVelY(int velY) {
+        this.velY = velY;
+    }
+    public void setID(ID id) {
+        this.id = id;
+    }
 //getters
-	public int getX() {
-		return x;
-	}
-	public int getY() {
-		return y;
-	}
-	public int getVelX() {
-		return velX;
-	}
-	public int getVelY() {
-		return velY;
-	}
-	public ID getID() {
-		return id;
-	}
-	
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public int getVelX() {
+        return velX;
+    }
+    public int getVelY() {
+        return velY;
+    }
+    public ID getID() {
+        return id;
+    }
+    
 }
--- FinalProject/src/defaultPackage/Player.java
+++ FinalProject/src/defaultPackage/Player.java
@@ -2,23 +2,38 @@
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+import java.io.*;
+import javax.imageio.*;
+
 
 public class Player extends GameObject {
+    private BufferedImage img;
+    
+    public Player(int x, int y, ID id, String filepath) {
+        super(x, y, id);
+        try
+        {
+            img = ImageIO.read(new File(filepath));
+        }
+        catch(Exception e){System.out.println(e.toString());}
+    }
 
-	public Player(int x, int y, ID id) {
-		super(x, y, id);
-	}
+    
+    public void tick() {
+        x += velX;
+        y += velY;
+    }
 
-	
-	public void tick() {
-		x += velX;
-		y += velY;
-	}
-
-	
-	public void render(Graphics g) {
-		g.setColor(Color.white);
-		g.fillRect(x,y,32,32);
-	}
+    
+    public void render(Graphics g) {
+        g.setColor(Color.white);
+        g.drawImage(img, x, y, 32, 32, null);
+        g.drawRect(x, y, 32, 32);
+    }
 
 }
